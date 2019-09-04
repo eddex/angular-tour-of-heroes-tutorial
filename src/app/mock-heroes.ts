e4d5b42257124384cb9e27f0b92fb45058dcf9cb
eddex <eddex@outlook.com>
2019-09-04 13:49:20 +0200

    angular tour of heroes tutorial part 0 - 4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Vreni' },
+  { id: 12, name: 'Luzia' },
+  { id: 13, name: 'Konstanze' },
+  { id: 14, name: 'Uschi' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
